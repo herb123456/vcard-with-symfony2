ac4555d82b8672d4df7f5545c8adb593959d2f14
herb123456 <herb123456>
2014-05-01 16:31:00 +0800

    Add doctrine extensions and captcha bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Herb\Vcard\ContactBundle\HerbVcardContactBundle(),
+            new Gregwar\CaptchaBundle\GregwarCaptchaBundle(),
             new Herb\Vcard\DefaultBundle\HerbVcardDefaultBundle(),
         );
 
